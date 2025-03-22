3cdb5bdd75948d850cb19c961f3b00237cc0c010
Ralph Haddad <ralphhaddad777@gmail.com>
2025-03-21 17:37:30 -0700

    Update GitHub Actions workflow for deployment and modify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Enable static exports
+  output: 'export',
   images: {
-    unoptimized: true, // Required for static export
-  }
-};
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
